a71edaca4dfe296f8a72dc8e7a5c9220d6c01035
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-07-15 15:27:58 -0400

    Integracion con los iconos de bootstrap

--- Dataflow.Client/obj/Debug/net8.0/Dataflow.Client.AssemblyInfo.cs
+++ Dataflow.Client/obj/Debug/net8.0/Dataflow.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88103b5ad3422d46fea4d16001563def55addee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecdf815aa6b3d25481688764ca426a5fc36ad7a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
