4255f076f4b8e034629a04c9903363ad162e7476
Nelson Pino <devsoftware.etiquetas@gmail.com>
2024-07-18 17:09:31 -0400

    mejora en el datagrid de data de ponches, agrupar por empleado, menun de empleado y el footer.

--- Dataflow.Client/obj/Debug/net8.0/Dataflow.Client.AssemblyInfo.cs
+++ Dataflow.Client/obj/Debug/net8.0/Dataflow.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ffa96f14b5b8089220114413672e688a5e0a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e24fd886c854597d1d746eab5a5b661d9cdfe5f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dataflow.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
